2b5cffd90829eb903f36ca39a8cc455c3e83926c
Kareem Hamdy Mohamed <115502314+kariMMeshal@users.noreply.github.com>
2025-05-02 15:42:54 +0300

    Update 0178-rank-scores.py

--- 0178-rank-scores/0178-rank-scores.py
+++ 0178-rank-scores/0178-rank-scores.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+# Use .rank() assigns ranks to scores:
+# - axis=0: apply method on columns
+# - method='dense': same values get the same rank, next rank is consecutive
+# - ascending=False: higher scores get lower rank numbers (1 is highest)
     scores['rank'] = scores['score'].rank(axis = 0,method = 'dense', ascending = False)
     scores.sort_values('rank', ascending = True, inplace = True)
     return(scores[['score' , 'rank']])
