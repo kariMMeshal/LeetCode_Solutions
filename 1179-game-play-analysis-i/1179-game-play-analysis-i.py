a02774ef4fbadde0943b07cf75f5e063075bd5b0
Kareem Hamdy Mohamed <115502314+kariMMeshal@users.noreply.github.com>
2025-05-08 15:36:23 +0300

    Time: 262 ms (98.64%), Space: 67.9 MB (56.29%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.py
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
-    activity.sort_values(by='event_date' , inplace=True)
-    activity.drop_duplicates(subset='player_id' , inplace=True)
-    activity.rename(columns={'event_date':'first_login'} , inplace=True)
-    return(activity[['player_id' , 'first_login']])
+    result  = activity.groupby('player_id')['event_date'].min().reset_index()
+    result.rename(columns={'event_date':'first_login'} , inplace=True)
+    return (result)
