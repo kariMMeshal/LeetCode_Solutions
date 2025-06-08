401e13ef02d2959ddb3a52922d9e505ae3a8068b
Kareem Hamdy Mohamed <115502314+kariMMeshal@users.noreply.github.com>
2025-06-08 19:53:44 +0300

    Time: 322 ms (14.18%), Space: 67.6 MB (97.21%) - LeetHub

--- 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.py
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.py
@@ -2,6 +2,5 @@ import pandas as pd
 
 def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
 
-    teacher = teacher.groupby('teacher_id')['subject_id'].nunique().reset_index()
-    teacher.rename(columns={'subject_id':'cnt'} , inplace=True)   
+    teacher = teacher.groupby('teacher_id')['subject_id'].nunique().reset_index(name='cnt')
     return(teacher)
